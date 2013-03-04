9cc790879316cd93e7f4f76ac07e5d160a71f07a
Ilya Ivensky <ilya.ivensky@gmail.com>
2013-03-04 07:48:24 -0500

    matlab engine integration for quadprog

--- ML/support_vector_machine.h
+++ ML/support_vector_machine.h
@@ -1,4 +1,4 @@
-#ifndef _SVM_H
+﻿#ifndef _SVM_H
 #define _SVM_H
 
 #include "matrix.h"
@@ -7,7 +7,6 @@
 #include <stdio.h>
 #include <string.h>
 #include "engine.h"
-#define  BUFSIZE 256
 
 //#pragma comment (lib,"C:/Program Files (x86)/MATLAB/R2012a Student/extern/lib/win32/microsoft/libeng.lib")
 //#pragma comment (lib,"C:/Program Files (x86)/MATLAB/R2012a Student/extern/lib/win32/microsoft/libmx.lib")
@@ -16,24 +15,247 @@
 namespace SVM {
 
 template <class T>
-void learn(const Matrix<T> & x, const Matrix<T> & y)
+signed label(const T & r)
 {
-	Engine *ep;
-	mxArray *T = NULL, *result = NULL;
-//	char buffer[BUFSIZE+1];
-	double time[10] = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 };
+	return r > 0 ? 1 : -1;
+}
 
-	/*
-	 * Call engOpen with a NULL string. This starts a MATLAB process 
-     * on the current host using the command "matlab".
-	 */
-	if (!(ep = engOpen(""))) {
-		fprintf(stderr, "\nCan't start MATLAB engine\n");
-		return;// EXIT_FAILURE;
+
+template <class T>
+unsigned learn(Engine * ep, const Matrix<T> & x_non_scaled, const Matrix<T> & y, vector<T> & w)
+{	
+	const double inf = std::numeric_limits<T>::infinity();
+	const double zero = 0.0;
+	const double minus_one = -1.0;
+
+	/****************************************************** 
+	* Remove x0 and scale
+	*******************************************************/
+
+	Matrix<T> x(x_non_scaled.row, x_non_scaled.col - 1);
+	for (unsigned r = 0; r < x_non_scaled.row; ++r)
+		for (unsigned c = 1; c < x_non_scaled.col; ++c)
+			x[r][c - 1] = x_non_scaled[r][c];
+
+	x.scale(-1.0, 1.0);
+	
+	/***************************************************************************
+	* Create H (quadr matrix)
+	* The number of rows and columns in H must equal the number of elements of f
+	*****************************************************************************/
+#if 1
+	Matrix<T> quadr(x.row, x.row);
+	for (unsigned i = 0; i < x.row; ++i)
+		for (unsigned j = 0; j < x.row; ++ j)
+			quadr[i][j] = y[i][0] * y[j][0] * dot_product(x[i], x[j]);
+#else
+	Matrix<T> yty = y.multiply_by_transposed(y);
+	Matrix<T> xtx = x.multiply_by_transposed(x);
+	Matrix<T> quadr = yty * xtx;
+#endif
+
+	if (debug && x.row <= 10)
+		cerr << "quadr:" << endl << quadr << endl;
+
+	T * buff_H = (T*)malloc(sizeof(T) * quadr.row * quadr.col);
+	
+	for (unsigned r = 0; r < quadr.row; ++r)
+		for (unsigned c = 0; c < quadr.col; ++c)
+			buff_H[(r * quadr.col) + c] = quadr[r][c];
+
+	mxArray * h = mxCreateDoubleMatrix(quadr.row, quadr.col, mxREAL);
+	memcpy((void *)mxGetPr(h), (void *)buff_H,  quadr.row * quadr.col * sizeof(T));
+	delete buff_H;
+
+	engPutVariable(ep, "H", h);
+
+	/***********************************************************************************
+	* Create f 
+	* Vector of doubles. Represents the linear term in the expression 1/2*x'*H*x + f'*x.
+	* Setting it to '-1'
+	* The number of rows and columns in H must equal the number of elements of f
+	************************************************************************************/
+	T * buff_f = (T*)malloc(sizeof(T) * quadr.row);
+	for (unsigned r = 0; r < quadr.row; ++r)
+		buff_f[r] = minus_one;
+	
+	mxArray * f = mxCreateDoubleMatrix(quadr.row, 1, mxREAL);
+	memcpy((void *)mxGetPr(f), (void *)buff_f, quadr.row * sizeof(T));
+	delete buff_f;
+
+	engPutVariable(ep, "f", f);
+
+	/****************************************************************************************
+	* Create A and b (empty)
+	* A - matrix of doubles. Represents the linear coefficients in the constraints A*x ≤ b.
+	* b - vector of doubles. Represents the constant vector in the constraints A*x ≤ b.
+	****************************************************************************************/
+	mxArray * a =  mxCreateDoubleMatrix(0, 0, mxREAL);
+	mxArray * b =  mxCreateDoubleMatrix(0, 0, mxREAL);
+
+	engPutVariable(ep, "A", a);
+	engPutVariable(ep, "b", b);
+
+	/***************************************************************************************
+	* Create Aeq 
+	* Matrix of doubles. Represents the linear coefficients in the constraints Aeq*x = beq
+	* The number of rows and columns in Aeq must be the same as the number of elements of f
+	****************************************************************************************/
+	T * buff_aeq = (T*)malloc(sizeof(T) * quadr.row * quadr.col);
+
+	for (unsigned r = 0; r < quadr.row; ++r)
+		for (unsigned c = 0; c < quadr.col; ++c)
+			buff_aeq[(r * quadr.col) + c] = y[r][0];
+
+	mxArray * aeq =  mxCreateDoubleMatrix(quadr.row, quadr.col, mxREAL);
+	memcpy((void *)mxGetPr(aeq), (void *)buff_aeq, quadr.row * quadr.col * sizeof(T));
+	delete buff_aeq;
+
+	engPutVariable(ep, "Aeq", aeq);
+	
+	/***********************************************************************************
+	* Create beq
+	* Vector of doubles. Represents the constant vector in the constraints Aeq*x = beq.
+	* The number of rows in Aeq must be the same as the number of elements of beq
+	************************************************************************************/
+	T * buff_beq = (T*)malloc(sizeof(T) * x.row);	
+	for (unsigned c = 0; c < quadr.row; ++c)
+		buff_beq[c] = zero;
+
+	mxArray * beq =  mxCreateDoubleMatrix(1, quadr.row, mxREAL);
+
+	memcpy((void *)mxGetPr(beq), (void *)buff_beq, quadr.row * sizeof(T));
+	delete buff_beq;
+
+	engPutVariable(ep, "beq", beq);
+	
+	/**************************************************************************************
+	* Create lb and ub
+	* Vectors of doubles. Represent the lower and upper bounds elementwise in lb ≤ x ≤ ub.
+	***************************************************************************************/
+	T * buff_lb = (T*)malloc(sizeof(T) * quadr.col);
+	T * buff_ub = (T*)malloc(sizeof(T) * quadr.col);
+	
+	for (unsigned c = 0; c < quadr.col; ++c)
+	{
+		buff_lb[c] = zero;
+		buff_ub[c] = 2000;//inf;
 	}
-	else
-		fprintf(stderr, "\nStarted MATLAB engine\n");
+
+	mxArray * lb =  mxCreateDoubleMatrix(1, quadr.col, mxREAL);
+	memcpy((void *)mxGetPr(lb), (void *)buff_lb, quadr.col * sizeof(T));
+	delete buff_lb;
+	
+	mxArray * ub =  mxCreateDoubleMatrix(1, quadr.col, mxREAL);
+	memcpy((void *)mxGetPr(ub), (void *)buff_ub, quadr.col * sizeof(T));
+	delete buff_ub;
+
+	engPutVariable(ep, "lb", lb);
+	engPutVariable(ep, "ub", ub);
+
+	/*******************************************************************************
+	* Creating x0
+	* Vector of doubles. Optional. The initial point for some quadprog algorithms.
+	*******************************************************************************/
+
+	mxArray * x0 =  mxCreateDoubleMatrix(0, 0, mxREAL);
+	engPutVariable(ep, "x0", x0);
+	
+    /*******************************************************
+	* Creating options
+	********************************************************/
+	//engEvalString(ep, "options=optimset('Algorithm','interior-point-convex')");
+	//engPutVariable(ep, "options", "optimset('Algorithm',interior-point-convex)");
+	//if (debug)
+		//_getch();
+	 
+
+	/*****************************************
+	* We well read results from this
+	******************************************/
+
+	mxArray * result = NULL, * fval = NULL, *exitflag = NULL;
+
+	engPutVariable(ep, "x", result);
+	engPutVariable(ep, "fval", fval);
+	engPutVariable(ep, "exitflag", exitflag);
+
+	// Evaluate quadprog
+	engEvalString(ep, "[x,fval,exitflag] = quadprog(H,f,A,b,Aeq,beq,lb,ub,x0,optimset('Algorithm','interior-point-convex'))");
+	
+	if (debug)
+		_getch();
+	 
+	vector<T> alpha(x.row, 0.0);
+	unsigned sv_count = 0;
+	if (debug)
+		printf("\nRetrieving results...\n");
+	if ((result = engGetVariable(ep,"x")) == NULL)
+	    printf("Oops! You didn't create a variable 'x' in Matlab workspace.\n\n");
+	else {
+		if (debug && false)
+		{
+			printf("'x' is class %s\t\n", mxGetClassName(result));
+			double * res = mxGetPr(result);
+			cerr << *res << endl;
+		}
+		T * buff_res = (T*)malloc(sizeof(T) * x.row);	
+		memcpy((void *)buff_res, (void *)mxGetPr(result), x.row * sizeof(T));
+
+		for (unsigned i = 0; i < x.row; ++i)
+		{
+			alpha[i] = buff_res[i];
+			if (abs(alpha[i]) > 0.001)
+				++sv_count;
+		}
+
+		delete buff_res;
+
+		if (debug)
+			cerr << "result is:" << endl << alpha << endl;
+	}
+
+#if 0
+	if ((fval = engGetVariable(ep,"fval")) == NULL)
+	    printf("Oops! You didn't create a variable 'fval' in Matlab workspace.\n\n");
+	else {
+		printf("'fval' is class %s\t\n", mxGetClassName(fval));
+		double * res = mxGetPr(fval);
+		cerr << *res << endl;
+	}
+#endif
+
+	mxDestroyArray(result);
+	mxDestroyArray(fval);
+	mxDestroyArray(exitflag);
+	mxDestroyArray(h);
+	mxDestroyArray(a);
+	mxDestroyArray(b);
+	mxDestroyArray(aeq);
+	mxDestroyArray(beq);
+	mxDestroyArray(lb);
+	mxDestroyArray(ub);
+	mxDestroyArray(x0);
+
+	vector<T> w_sv(x.col, 0.0);
+	unsigned sv = x.row;
+	for (unsigned r = 0; r < x.row; ++r)
+		for (unsigned c = 0; c < x.col; ++c)
+		{
+			if (abs(alpha[r]) > 0.001)
+			{
+				w_sv[c] += alpha[r] * y[r][0] * x[r][c];
+				sv = r;
+			}
+		}
+
+	w[0] = y[sv][0] - dot_product(w_sv, x[sv]);
+	for (unsigned c = 0; c < x.col; ++c)
+		w[c + 1] = w_sv[c];
+
+	return sv_count;
 }
 
 } // namespace
+
 #endif
