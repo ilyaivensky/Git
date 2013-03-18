0ac7419e581b366103b3c27e672b34145fe3923b
Ilya Ivensky <ilya.ivensky@gmail.com>
2013-03-18 11:59:55 -0400

    runtime optimization

--- ML/linear_solutions.h
+++ ML/linear_solutions.h
@@ -11,8 +11,11 @@ template <class T>
 Matrix<T> linear_pseudoinverse_solution(const Matrix<T> & x, const Matrix<T> & y, T lambda = 0.0)
 {
 	Matrix<T> xtx = x.xtx();
-	Matrix<T> reg = Matrix<T>::diag(xtx.row, lambda);
-	xtx += reg;
+	if (lambda != 0.0)
+	{
+		Matrix<T> reg = Matrix<T>::diag(xtx.row, lambda);
+		xtx += reg;
+	}
 	Matrix<T> inverse = xtx.invert();
 	
 	Matrix<T> h = inverse.multiply_by_transposed(x);
